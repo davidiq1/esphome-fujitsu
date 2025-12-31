652fa6d81d9eb22b91ec138114a545ea84fdf6cc
davidiq1 <111751137+davidiq1@users.noreply.github.com>
2025-12-31 15:54:00 +0100

    Update climate.py

--- components/fujitsu_heat_pump/climate.py
+++ components/fujitsu_heat_pump/climate.py
@@ -6,21 +6,19 @@ from esphome.const import CONF_ID
 # Definimos las dependencias
 DEPENDENCIES = ['uart']
 
-# Creamos el espacio de nombres (namespace) fujitsu
-# Esto debe coincidir con el "namespace fujitsu" que suele estar en los .h/.cpp
+# Espacio de nombres
 fujitsu_ns = cg.esphome_ns.namespace('fujitsu')
 FujitsuClimate = fujitsu_ns.class_('FujitsuClimate', climate.Climate, cg.Component)
 
-# Esquema de configuración para el YAML
+# ESQUEMA CORREGIDO PARA VERSIONES MODERNAS
 CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(FujitsuClimate),
 }).extend(uart.UART_DEVICE_SCHEMA).extend(cv.COMPONENT_SCHEMA)
 
 def to_code(config):
-    # Esta función le dice a ESPHome qué archivos compilar
     var = cg.new_Pvariable(config[CONF_ID])
     
-    # Registramos los archivos .cpp para que el compilador los incluya
+    # Asegúrate de que estos nombres coincidan con tus archivos en GitHub
     cg.add_sources("FujiHeatPump.cpp", "FujitsuClimate.cpp")
     
     yield cg.register_component(var, config)
